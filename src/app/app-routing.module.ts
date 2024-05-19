f6edfbf49a381e77472ee79e857f955ecfd06b93
MohamedSalah111500 <mohamedsalah111500@gmail.com>
2024-05-20 01:52:32 +0300

    work on test builder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { Page404Component } from './extrapages/page404/page404.component';
 
 const routes: Routes = [
   // { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
-  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), canActivate: [AuthGuard] },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule) },
   { path: 'crypto-ico-landing', component: CyptolandingComponent },
   { path: '**', component: Page404Component },
 ];
